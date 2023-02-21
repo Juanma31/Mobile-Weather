c61fd604579298fa8d875aa05e789db5fc6d12ac
Juanma31 <65458652+Juanma31@users.noreply.github.com>
2023-02-21 21:11:55 +0000

    Update utils.py

--- Twilio_Scripts/utils.py
+++ Twilio_Scripts/utils.py
@@ -58,34 +58,21 @@ def send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,df,query):
 
     client = Client(account_sid, auth_token)
     
-   """ if(df.empty):
+    if(df.empty):
         message = client.messages \
                             .create(
                                 body='\nHello! \n\n\n Today '+ input_date +' in ' + query +' no rain is expected',
-                                from_=PHONE_NUMBER,
-                                to='+447568279452'
+                                from_='whatsapp:+14155238886',
+                                to='whatsapp:+447568279452'
                             )
     else:
         message = client.messages \
                             .create(
                                 body='\nHello! \n\n\n The weather forecast today '+ input_date +' in ' + query +' is : \n\n\n ' + str(df),
-                                from_=PHONE_NUMBER,
-                                to='+447568279452'
-                            )"""
-	if(df.empty):
-    	    message = client.messages \
-        	                    .create(
-            	                    body='\nHello! \n\n\n Today '+ input_date +' in ' + query +' no rain is expected',
-                	                from_='whatsapp:+14155238886',
-                    	            to='whatsapp:+447568279452'
-                        	    )
-    	else:
-        	message = client.messages \
-            	                .create(
-                	                body='\nHello! \n\n\n The weather forecast today '+ input_date +' in ' + query +' is : \n\n\n ' + str(df),
-                    	            from_='whatsapp:+14155238886',
-                        	        to='whatsapp:+447568279452'
-                            	)
+                                from_='whatsapp:+14155238886',
+                                to='whatsapp:+447568279452'
+                            )
+	
   
 
     return message.sid
